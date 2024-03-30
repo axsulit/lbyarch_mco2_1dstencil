fafef73485b86d207a3d8bec93f3c2b824576cc2
Anne <anne_sulit@dlsu.edu.ph>
2024-03-30 23:30:06 +0800

    feat: getTimestamp(), getExecutionTime()
    already added code using the new functions with c kernel

--- lbyarch_mco2/main.c
+++ lbyarch_mco2/main.c
@@ -2,8 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#include <math.h> 
+#include <math.h>
+#include <time.h>
 
+/**
+ * @brief Retrieves the current high-resolution performance counter value.
+ * 
+ * @return The current high-resolution performance counter value.
+ */
+LARGE_INTEGER getTimestamp() {
+	LARGE_INTEGER time;
+	QueryPerformanceCounter(&time);
+	return time;
+}
+
+/**
+ * @brief Calculates the execution time in seconds based on the start and end timestamps.
+ * 
+ * @param start The start timestamp.
+ * @param end The end timestamp.
+ * @return The execution time in seconds.
+ */
+double getExecutionTime(LARGE_INTEGER start, LARGE_INTEGER end) {
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+	return (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+}
 
 /**
  * @brief Computes the 1d stencil operation on the input array X, stores the result in Y, and displays it.
@@ -12,7 +36,7 @@
  * @param X Pointer to the input array.
  * @param Y Pointer to the output array to store the result.
  */
-void dstencil(int n, double* X, double* Y) {
+void c_dStencil(int n, double* X, double* Y) {
 	printf("(C) Output Y -> ");
 	int max = (n < 10) ? n : 10;
 
@@ -23,7 +47,7 @@ void dstencil(int n, double* X, double* Y) {
 		}
 	}
 	for (int i = 0; i < max; i++) {
-		printf("%.4f%s", Y[i], (i + 1 != max) ? ", " : " . . .");
+		printf("%.4f%s", Y[i], (i + 1 != max) ? ", " : " . . .\n");
 	}
 }
 
@@ -35,7 +59,7 @@ void dstencil(int n, double* X, double* Y) {
  * @return A pointer to the dynamically allocated array of random double values,
  *         or NULL if memory allocation fails.
  */
-double* gen_arr(int size, int seed) {
+double* genArr(int size, int seed) {
 	double* arr = malloc(size * sizeof(double));
 	if (arr == NULL) {
 		printf("malloc failed!\n");
@@ -53,11 +77,11 @@ double* gen_arr(int size, int seed) {
 
 
 int main() {
-	int n = 20; //1048576 [2^20], 16777216 [2^24], 268435456 [2^28] 
-
+	int n = 1048576; //1048576 [2^20], 16777216 [2^24], 268435456 [2^28] 
+	
 	printf("Generating random elements for array of size %d . . .\n", n);
+	double* X = genArr(n, 96);
 
-	double* X = gen_arr(n, 96);
 	n = n - 6;
 	double* Y = malloc(n * sizeof(double));
 
@@ -70,9 +94,15 @@ int main() {
 
 	printf("\n");
 
-	dstencil(n, X, Y);
+	LARGE_INTEGER cstart = getTimestamp();
+	c_dStencil(n, X, Y);
+	LARGE_INTEGER cend = getTimestamp();
+
+	double ctime = getExecutionTime(cstart, cend);
+	printf("(C) Execution time for 1D vector of size %d -> %lf seconds . . .\n", n, ctime);
 
 	free(X);
 	free(Y);
+
 	return 0;
 }
